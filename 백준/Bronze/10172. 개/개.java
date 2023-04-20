f79435a067cd7665bcc40526186f231807d63805
HyeyeonJ <113879120+HyeyonJ@users.noreply.github.com>
2023-04-20 13:32:43 +0900

    [Bronze V] Title: 개, Time: 120 ms, Memory: 13888 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,34 @@
+public class Main {
+    
+    public static void main(String[] args){
+/*
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+*/
+        // " 따옴표, \ 백슬레시를 표현하고 싶다면
+        // ("\\")
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+        
+        /* 그 외의 방법들
+		System.out.print(
+        "|\\_/|\n" + 
+		"|q p|   /}\n" + 
+		"( 0 )\"\"\"\\\n" + 
+		"|\"^\"`    |\n" + 
+		"||_/=\\\\__|");
+                         
+		System.out.printf("%s", "|\\_/|\n
+  		|q p|   /}\n
+ 		( 0 )\"\"\"\\\n
+		|\"^\"`    |\n
+		||_/=\\\\__|");
+        */
+    }
+}
