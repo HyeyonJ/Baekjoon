ad00afe87cb7a50a2e1aa37acfde0e0ce93841a7
HyeyeonJ <113879120+HyeyonJ@users.noreply.github.com>
2024-01-14 09:19:59 +0900

    [level Lv.0] Title: 대소문자 바꿔서 출력하기, Time: 44.64 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.js
+++ 프로그래머스/Lv.0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.js
@@ -1,4 +1,5 @@
 const readline = require('readline');
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -9,5 +10,15 @@ let input = [];
 rl.on('line', function (line) {
     input = [line];
 }).on('close',function(){
-    str = input[0];
+    
+    str = input[0].split('');
+    str.forEach((value, index) => {
+        if (value === value.toUpperCase()) {
+            str[index] = value.toLowerCase();
+        } else {
+            str[index] = value.toUpperCase();
+        }
+    });
+    
+    console.log(str.join(''));
 });
